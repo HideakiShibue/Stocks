186e62c6d3779cfed719700a3db776a38d519277
Shuta Shibue <shuta.shibue@gmail.com>
2022-06-09 11:48:15 +0900

    screener v1.2.2 added title

--- screen_csv.py
+++ screen_csv.py
@@ -14,8 +14,9 @@ def get_codes(file_path):
 def draw_chart(code):
     df = create_dataframe(code)
     df = df.tail(120)
+    plt.title(code)
     plt.plot(df["Close"])
-    plt.plot(df["MA200"], color='red')
+    plt.plot(df["MA25"], color='red')
     plt.plot(df["MA75"], color='blue')
     plt.gcf().autofmt_xdate()
 
@@ -26,7 +27,7 @@ def create_dataframe(code):
     df = pd.read_csv("data/" + code + ".csv", index_col=0, parse_dates=True)
     df.index.name = 'Date'
     print(code)
-    if len(df.index) < 100:
+    if len(df.index) < 180:
         return False
     Close = df["Close"]
     df["MA75"] = Close.rolling(75).mean()
@@ -42,7 +43,7 @@ for code in codes:
     if df is False:
         continue
     diff = df["MA75"].iloc[-1]/df["Close"].iloc[-1]
-    if diff > TORELANCE or 1/diff > TORELANCE:
+    if diff > TORELANCE or (1/diff) > TORELANCE:
         continue
 
     current_slope = df["MA75"].iloc[-1] - df["MA75"].iloc[-2]  # 直近の上昇
@@ -60,11 +61,11 @@ while len(matched):
     plt.clf()
     plt.figure(figsize=(25, 10))
     plt.subplots_adjust(wspace=0.2, hspace=0.1)  # (2)間隔指定
-    
-    for j in range(25):
+
+    for j in range(20):
         if len(matched) == 0:
             break
-        plt.subplot(5, 5, j+1)  # (3)グラフ描画位置の指定
+        plt.subplot(4, 5, j+1)  # (3)グラフ描画位置の指定
         draw_chart(matched.pop())
     plt.savefig(f'chart/{file}.png')
     file += 1
